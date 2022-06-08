747d94a3ad5ba396da03f7816cbb8788bc662b8a
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2022-06-08 14:51:38 -0400

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import {Link} from "react-router-dom";
 
 function Form() {
     const movie = {
@@ -11,13 +12,13 @@ function Form() {
 
     return (
         <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <form className="dsmovie-form">
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
+                        <input type="email" className="form-control" id="email"/>
                     </div>
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="score">Informe sua avaliação</label>
@@ -32,10 +33,12 @@ function Form() {
                     <div className="dsmovie-form-btn-container">
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
-                </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-            </div >
-        </div >
+                </form>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
